e2d903784f9e5b80d6cd7959d2b28c00542eb63d
raghad <raghadayyad32@gmail.com>
2026-04-21 23:05:01 +0300

    added a gallery section file

---
+++ src/GallerySection.jsx
@@ -0,0 +1,8 @@
+export default function GallerySection() {
+    return(
+        <>
+            <h2>Gallery</h2>
+            <p>Explore our gallery to see our products in action and get inspired:</p>
+        </>
+    )
+}
