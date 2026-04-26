e9b72c7a6e6e3772c90e7f325fb90e8e9d2ba902
amr.rashidy2020@gmail.com <mhmoudemad1>
2026-04-26 21:00:32 +0300

    created Amr's hero section

---
+++ src/AmrHeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AmrHeroSection = () => {
+    return (
+        <div>
+            <h2>this is Amr's Hero Section</h2>
+        </div>
+    );
+};
+
+export default AmrHeroSection;
